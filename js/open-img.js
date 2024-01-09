83c701a411e2eeb2778aa9f82aeb6f893a41d569
DainerCortes <dainer.ramirez2003@gmail.com>
2024-01-08 20:47:31 -0500

    Corrección en la carga de imagenes

--- js/open-img.js
+++ js/open-img.js
@@ -1,8 +1,8 @@
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', async function() {
     const image = document.getElementById('image-change')
     
     let extractUrl = localStorage.getItem('search-img')
     //console.log(extractUrl)
     image.src = extractUrl
 
-}
+})
